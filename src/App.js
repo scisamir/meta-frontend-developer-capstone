6ec0bce0c31da17ce87053fc9202c7ad24b98a0e
Samir Idris <samzidris11@gmail.com>
2023-05-08 18:04:58 +0100

    move booking form time state to parent component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import { useReducer } from 'react';
 import './App.css';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
@@ -8,20 +7,12 @@ import ConfirmedBooking from './components/confirmedBooking';
 import Footer from './components/Footer';
 
 function App() {
-  const updateTimes = (state, action) => {
-    return state;
-  };
-
-  const initializeTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
-
-  const [state, dispatch] = useReducer(updateTimes, initializeTimes);
-
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/bookings" element={<BookingsPage state={state} dispatch={dispatch} />} />
+        <Route path="/bookings" element={<BookingsPage />} />
         <Route path="/confirmed" element={<ConfirmedBooking />} />
       </Routes>
       <Footer />
--- src/components/BookingsPage.jsx
+++ src/components/BookingsPage.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { useReducer } from 'react';
 import BookingForm from './bookings/BookingForm'
 
-export default function BookingsPage({ state, dispatch }) {
+export default function BookingsPage() {
+  const updateTimes = (state, action) => {
+    return state;
+  };
+
+  const initializeTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+
+  const [state, dispatch] = useReducer(updateTimes, initializeTimes);
+
   return (
     <>
       <BookingForm state={state} dispatch={dispatch} />
