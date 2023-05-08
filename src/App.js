98db4fe1ffdf6732ad1c453db3a4d9c42765796e
Samir Idris <samzidris11@gmail.com>
2023-05-08 19:19:19 +0100

    add scroll to top

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import HomePage from './components/HomePage';
 import BookingsPage from './components/BookingsPage';
 import ConfirmedBooking from './components/confirmedBooking';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/bookings" element={<BookingsPage />} />
-        <Route path="/confirmed" element={<ConfirmedBooking />} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/bookings" element={<BookingsPage />} />
+          <Route path="/confirmed" element={<ConfirmedBooking />} />
+        </Routes>
+      </ScrollToTop>
       <Footer />
     </>
   );
